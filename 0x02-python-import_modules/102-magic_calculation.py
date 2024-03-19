991f76b4c0adb3b85d519a9d4d0e75385f67c17f
sunnytk6 <sunnytk6@gmail.com>
2024-03-19 14:22:44 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+# 102-magic_calculation.py
+# Brennan D Baraban <375@holbertonschool.com>
+
 
 def magic_calculation(a, b):
-
-    """Match bytecode provided by Holberton School."""
-
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
-    else:
-        return(sub(a, b))
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return (result)
