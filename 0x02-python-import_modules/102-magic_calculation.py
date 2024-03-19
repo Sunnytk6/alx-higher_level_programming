b88bda9039998c1e1cef4c3943a1719eb9b19f7b
sunnytk6 <sunnytk6@gmail.com>
2024-03-19 14:47:11 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,10 +2,18 @@
 from __future__ import print_function
 import sys
 
+def magic_calculation(a, b):
 
-def safe_function(fct, *args):
-    try:
-        res = fct(*args)
+    """Match bytecode provided by Holberton School."""
+
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
